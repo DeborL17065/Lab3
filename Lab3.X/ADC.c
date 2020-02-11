b427943a68227b1cbfad69ada6301afa32661e4a
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-02-11 13:07:56 -0600

    Update ADC.c
    LIBRERIA COMENTADA

--- Lab3.X/ADC.c
+++ Lab3.X/ADC.c
@@ -27,17 +27,17 @@ void CONF_ADC(void) {
     SENSOR1 = ADRESH;
     SENSOR1 = SENSOR1<<8;
     SENSOR1 |= ADRESL;
-    V_SENSOR1 =(SENSOR1*5.0)/1022;
-    sprintf(S1,"%3.2f",V_SENSOR1);
-    strcpy(val1,"SENSOR1:");
-    strcat(val1,S1);
+    V_SENSOR1 =(SENSOR1*5.0)/1022; //se pasa de 0-5
+    sprintf(S1,"%3.2f",V_SENSOR1); //se pasa de float a char
+    strcpy(val1,"SENSOR1:"); //se coloca el caracter dentro del char
+    strcat(val1,S1); //se concatenan ambos chars
     SerialSendString(val1); //DATOS A ENVIARA AL ARDUINO
   
     Lcd_Clear();
     LCD_XY(0,2);
     LCD_Cadena("S1:  S2:  S3:"); 
     LCD_XY(1,2);
-    LCD_Cadena(S1);
+    LCD_Cadena(S1);//manda el dato a la LCD
 
     ADCON0 = 0b01000101; //AN1
     ADCON1 = 0b10000000;
@@ -46,13 +46,13 @@ void CONF_ADC(void) {
     SENSOR2 = ADRESH;
     SENSOR2 = SENSOR2<<8;
     SENSOR2 |= ADRESL;
-    V_SENSOR2 =(SENSOR2*5.0)/1022;
-    sprintf(S2,"%3.2f",V_SENSOR2);
-    strcpy(val2,"SENSOR2:");
-    strcat(val2,S2);
+    V_SENSOR2 =(SENSOR2*5.0)/1022; //se pasa de 0-5
+    sprintf(S2,"%3.2f",V_SENSOR2); //se pasa de float a char
+    strcpy(val2,"SENSOR2:"); //se coloca el caracter dentro del char
+    strcat(val2,S2);         //se concatenan ambos chars
     SerialSendString(val2); //DATOS A ENVIARA AL ARDUINO
     LCD_XY(1,7);
-    LCD_Cadena(S2);
+    LCD_Cadena(S2); //manda el dato a la LCD
     
 }
 
