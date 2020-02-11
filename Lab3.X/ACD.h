d1c9153011a35d27dc011f50e9d48851ef8453ae
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-02-11 12:52:17 -0600

    Create ACD.h

---
+++ Lab3.X/ACD.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   ACD.h
+ * Author: debor
+ *
+ * Created on February 6, 2020, 11:24 PM
+ */
+
+#ifndef ACD_H
+#define	ACD_H
+
+uint16_t SENSOR1;
+uint16_t SENSOR2;
+float V_SENSOR1;
+float V_SENSOR2;
+char S1[5];
+char S2[5];
+char S3[5];
+char val1[12];
+char val2[12];
+ 
+
+
+void CONF_ADC(void);
+
+
+
+
+#endif	/* ACD_H */
+
